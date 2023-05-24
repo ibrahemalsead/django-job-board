6b3d0093e68b4e2711b00ba570737b8eb1a3bbf0
ibrahemalsead <ibrahem1999916@gmail.com>
2023-05-24 22:43:15 +0300

    add jop models

--- jop/models.py
+++ jop/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+JOP_TYPE = (
+    ('Full Time','Full Time'),
+    ('Part Time','Part Time'),
+    )
+class Jop(models.Model):
+    title =models.CharField(max_length=100)
+    #location 
+    jop_type=models.CharField(max_length=15,choices=JOP_TYPE)
+    description = models.TextField(max_length=1000)
+    published_at = models.DateTimeField(auto_now=True)
+    Vacancy=models.IntegerField(default=1)
+    salary= models.IntegerField(default=0)
+    experience =models.IntegerField(default=2) 
+
+
+    def __str__(self) :
+        return self.title
